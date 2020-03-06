5b2d978626448c4e34c36c897c1d6cd2ab105680
kevn ekwa <ekwabayard@gmail.com>
2020-03-06 18:16:05 +0100

    header component adding

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import React, {Component} from 'react';
-import {Route,Switch} from 'react-router-dom';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.component';
+
 import './App.css';
-import HomePage from './pages/homepage.component';
 
-const HatsPage=()=>(
-  <div><h1>HATS PAGE</h1>
-  </div>
-);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-class App extends Component{
- render() {
-   return (
-       <div className="App">
-       <Switch>
-
-           <Route
-               exact
-               path='/'
-               component={HomePage}/>
-
-           <Route
-               path='/hats'
-               component={HatsPage}/>
-
-
-       </Switch>
-
-       </div>
-   );
- }
+function App() {
+  return (
+    <div>
+        <Header/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
